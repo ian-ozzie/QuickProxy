4e72ff2ac79095c092867fb28ecb5a6869eb7936
Ian Oswald <ian@ozzie.id.au>
2008-06-10 13:30:20 +0000

    Build - Corrected date format in build-release.bat quickproxy-2008.03.11-fx.xpi - Updated for FireFox 3.0b5pre compatibility

--- build/build-release.bat
+++ build/build-release.bat
@@ -19,8 +19,8 @@ echo Removing previous build
 del /Q ..\release\xpi\*.xpi
 echo ---
 FOR /F "TOKENS=1* DELIMS= " %%A IN ('DATE/T') DO SET CDATE=%%B
-FOR /F "TOKENS=1,2 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET mm=%%B
-FOR /F "TOKENS=1,2 DELIMS=/ eol=/" %%A IN ('echo %CDATE%') DO SET dd=%%B
+FOR /F "TOKENS=1,2 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET dd=%%B
+FOR /F "TOKENS=1,2 DELIMS=/ eol=/" %%A IN ('echo %CDATE%') DO SET mm=%%B
 FOR /F "TOKENS=2,3 DELIMS=/ " %%A IN ('echo %CDATE%') DO SET yyyy=%%B
 SET release=quickproxy-%yyyy%.%mm%.%dd%-fx.xpi
 echo Created release name: %release%
